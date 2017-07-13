9215c5c3f0fefdc8b3dd90bd7c144232292c843d
Andrei Olteanu <a.olteanu198@yahoo.com>
2017-07-13 18:16:22 +0300

    started coding the app

---
+++ pokeDex/pokeDex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokeDex
+//
+//  Created by Mac on 13/07/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        
+        
+        
+    }
+    
+}
