b1fa75d5141f858f9647bb50007f64c35ec70658
Andrei Olteanu <a.olteanu198@yahoo.com>
2017-07-14 17:01:49 +0300

    PokeDex app done
    App which searches between 718 different pokemons and shows them with their abilities, the search engine is really fast

---
+++ pokeDex/pokeDex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokeDex
+//
+//  Created by Mac on 14/07/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
