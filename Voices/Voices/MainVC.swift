f0fdf9b8b8da9411216968019095df11dd144401
Andrei Olteanu <a.olteanu198@yahoo.com>
2017-07-21 12:48:32 +0300

    Voices working pretty good

--- Voices/Voices/MainVC.swift
+++ Voices/Voices/MainVC.swift
@@ -10,6 +10,8 @@ import UIKit
 import Speech
 import AVFoundation
 import Pulsator
+import  UserNotifications
+
 
 class MainVC: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
 
@@ -21,12 +23,13 @@ class MainVC: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     let pulsator = Pulsator()
 
     var audioPlayer: AVAudioPlayer!
-    //var recordingSession: AVAudioSession!
-    //var audioRecorder: AVAudioRecorder!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
     var settings = [String : Int]()
     
     @IBOutlet weak var recordButtonPressed: UIButton!
     
+    var pathNSURL: NSURL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,90 +40,111 @@ class MainVC: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
         pulsator.backgroundColor = UIColor(red:0.24, green:0.65, blue:0.73, alpha:1.0).cgColor
         pulsator.radius = 90.0
         
-//        recordingSession = AVAudioSession.sharedInstance()
-//        do {
-//            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
-//            try recordingSession.setActive(true)
-//            recordingSession.requestRecordPermission() { [unowned self] allowed in
-//                DispatchQueue.main.async {
-//                    if allowed {
-//                        print("Allow")
-//                    } else {
-//                        print("Dont Allow")
-//                    }
-//                }
-//            }
-//        } catch {
-//            print("failed to record!")
-//        }
-//        
-//        // Audio Settings
-//        
-//        settings = [
-//            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-//            AVSampleRateKey: 12000,
-//            AVNumberOfChannelsKey: 1,
-//            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-//        ]
-
+        recordingSession = AVAudioSession.sharedInstance()
+        do {
+          try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+              DispatchQueue.main.async {
+                    if allowed {
+                        print("Allow")
+                    } else {
+                        print("Dont Allow")
+                    }
+                }
+            }
+      } catch {
+            print("failed to record!")
+        }
+        
+        // Audio Settings
+        
+      settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        pathNSURL = self.directoryURL()
         
         
         
     }
     
-//    func directoryURL() -> URL? {
-//        let fileManager = FileManager.default
-//        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
-//        let documentDirectory = urls[0] as NSURL
-//        let soundURL = documentDirectory.appendingPathComponent("andrei.m4a")
-//        print(soundURL)
-//        return soundURL as URL?
-//    }
-//    
-//    func startRecording() {
-//        let audioSession = AVAudioSession.sharedInstance()
-//        do {
-//            audioRecorder = try AVAudioRecorder(url: self.directoryURL()! as URL,
-//                                                settings: settings)
-//            audioRecorder.delegate = self
-//            audioRecorder.prepareToRecord()
-//        } catch {
-//            finishRecording(success: false)
-//        }
-//        do {
-//            try audioSession.setActive(true)
-//            audioRecorder.record()
-//        } catch {
-//        }
-//    }
-//
-//    func finishRecording(success: Bool) {
-//        audioRecorder.stop()
-//        if success {
-//            print(success)
-//        } else {
-//            audioRecorder = nil
-//            print("Somthing Wrong.")
-//        }
-//    }
-//    
-////    @IBAction func click_AudioRecord(_ sender: AnyObject) {
-////        if audioRecorder == nil {
-////            self.btnAudioRecord.setTitle("Stop", for: UIControlState.normal)
-////            self.btnAudioRecord.backgroundColor = UIColor(red: 119.0/255.0, green: 119.0/255.0, blue: 119.0/255.0, alpha: 1.0)
-////            self.startRecording()
-////        } else {
-////            self.btnAudioRecord.setTitle("Record", for: UIControlState.normal)
-////            self.btnAudioRecord.backgroundColor = UIColor(red: 221.0/255.0, green: 27.0/255.0, blue: 50.0/255.0, alpha: 1.0)
-////            self.finishRecording(success: true)
-////        }
-////    }
+    func directoryURL() -> NSURL? {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls[0] as NSURL
+        let soundURL = documentDirectory.appendingPathComponent("andrei.m4a")
+        print(soundURL!)
+        return soundURL as NSURL?
+    }
+    
+    func startRecording() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            audioRecorder = try AVAudioRecorder(url: pathNSURL as URL,
+                                                settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+        } catch {
+            finishRecording(success: false)
+        }
+        do {
+            try audioSession.setActive(true)
+            audioRecorder.record()
+        } catch {
+        }
+    }
+
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        if success {
+            print(success)
+        } else {
+            audioRecorder = nil
+            print("Somthing Wrong.")
+        }
+    }
+
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         if !flag {
             finishRecording(success: false)
+            
         }
         pulsator.stop()
+        
+        if let path = pathNSURL {
+            
+            do {
+                print("***************************",path)
+                let sound = try AVAudioPlayer(contentsOf: path as URL)
+                self.audioPlayer = sound
+                self.audioPlayer.delegate = self
+                sound.play()
+            } catch {
+                print("Error!!!!")
+            }
+            
+            let recognizer = SFSpeechRecognizer()
+            let request = SFSpeechURLRecognitionRequest(url: path as URL)
+            recognizer?.recognitionTask(with: request) { (result, error) in
+                
+                if let error = error {
+                    print("There was an error: \(error)")
+                } else {
+                    self.transcriptionTextField.text = result?.bestTranscription.formattedString
+                }
+            }
+            
+        }
+        
+        pathNSURL = self.directoryURL()
+        print("NEW: ",pathNSURL)
+        self.audioRecorder = nil
+        
     }
     
     
@@ -140,48 +164,29 @@ class MainVC: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
         SFSpeechRecognizer.requestAuthorization { authStatus in
             if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
                 
-                
-                
-                
-                // Handling the media file, maybe here we will handle our voice recording
-                if let path = Bundle.main.url(forResource: "andrei1", withExtension: "m4a") {
-                    do {
-                        let sound = try AVAudioPlayer(contentsOf: path)
-                        self.audioPlayer = sound
-                        self.audioPlayer.delegate = self
-                        sound.play()
-                    } catch {
-                        print("Error!!!!")
-                    }
+                if self.audioRecorder == nil {
                     
-                    let recognizer = SFSpeechRecognizer()
-                    let request = SFSpeechURLRecognitionRequest(url: path)
-                    recognizer?.recognitionTask(with: request) { (result, error) in
-                    
-                        if let error = error {
-                                print("There was an error: \(error)")
-                        } else {
-                            self.transcriptionTextField.text = result?.bestTranscription.formattedString
-                        }
-                    }
-                    
+                    self.startRecording()
+                } else {
+                    self.finishRecording(success: true)
                 }
             }
+                
+                
+                
+                //if let path = Bundle.main.url(forResource: "andrei1", withExtension: "m4a") {
+                
+            }
         }
-        
-    }
-    
-    
+  
     @IBAction func playBtnPressed(_ sender: Any) {
-        activitySpinner.isHidden = true
-        activitySpinner.startAnimating()
+        
+        //activitySpinner.isHidden = true
+        //activitySpinner.startAnimating()
+        
         requestSpeechAuth()
         pulsator.start()
     }
-    
-    
-
-    
 }
 
 
